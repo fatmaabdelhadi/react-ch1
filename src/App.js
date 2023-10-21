4e69dbaca42c7bb378c165c748d90623920c4db6
Fatma AbdelHadi <fatmaabdelhadi2002@gmail.com>
2023-10-21 16:02:51 +0300

    trial

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/Welcome';
 
 function App() {
 
-  const myname = "Fatoom"
+  // const myname = "Fatoom"
 
-  function greeting(name){
-    if(name){
-      return <h2>Hello, {name}!</h2>
-    }
-    return <h2>Hello, Guest!</h2>
-  }
+  // function greeting(name){
+  //   if(name){
+  //     return <h2>Hello, {name}!</h2>
+  //   }
+  //   return <h2>Hello, Guest!</h2>
+  // }
 
   return (
     <div className='App'>
-      <h1>Hello React App</h1>
-      {greeting(myname)}
+      <Welcome />
     </div>
   );
 }
